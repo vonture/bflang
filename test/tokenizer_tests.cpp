fb8b4297dd69f1ee3854df866e42d72690eb1946
Geoff Lang <geoff@vonture.net>
2017-09-07 17:58:56 -0400

    Add more tokenizer tests.

--- test/tokenizer_tests.cpp
+++ test/tokenizer_tests.cpp
@@ -20,12 +20,12 @@ TEST(tokenizer, null_program) { ASSERT_EQ(nullptr, bf_tokenize(nullptr, nullptr,
 
 TEST(tokenizer, null_tokens) {
     int count = 0;
-    ASSERT_FALSE(bf_tokens_get_token_count(nullptr, &count));
+    EXPECT_FALSE(bf_tokens_get_token_count(nullptr, &count));
 
     bf_token token;
-    ASSERT_FALSE(bf_tokens_get_tokens(nullptr, 0, 1, &token));
+    EXPECT_FALSE(bf_tokens_get_tokens(nullptr, 0, 1, &token));
 
-    ASSERT_FALSE(bf_tokens_destroy(nullptr));
+    EXPECT_FALSE(bf_tokens_destroy(nullptr));
 }
 
 TEST(tokenizer, invalid_offsets) {
@@ -33,14 +33,14 @@ TEST(tokenizer, invalid_offsets) {
     ASSERT_NE(nullptr, tokens);
 
     bf_token tokens_array[4];
-    ASSERT_TRUE(bf_tokens_get_tokens(tokens.get(), 0, 1, tokens_array));
-    ASSERT_TRUE(bf_tokens_get_tokens(tokens.get(), 0, 0, tokens_array));
-    ASSERT_FALSE(bf_tokens_get_tokens(tokens.get(), -1, 1, tokens_array));
-    ASSERT_FALSE(bf_tokens_get_tokens(tokens.get(), 0, 3, tokens_array));
-    ASSERT_FALSE(bf_tokens_get_tokens(tokens.get(), 1, 2, tokens_array));
-    ASSERT_FALSE(bf_tokens_get_tokens(tokens.get(), 2, 1, tokens_array));
-    ASSERT_FALSE(bf_tokens_get_tokens(tokens.get(), 1, -1, tokens_array));
-    ASSERT_FALSE(bf_tokens_get_tokens(tokens.get(), 1, 1, nullptr));
+    EXPECT_TRUE(bf_tokens_get_tokens(tokens.get(), 0, 1, tokens_array));
+    EXPECT_TRUE(bf_tokens_get_tokens(tokens.get(), 0, 0, tokens_array));
+    EXPECT_FALSE(bf_tokens_get_tokens(tokens.get(), -1, 1, tokens_array));
+    EXPECT_FALSE(bf_tokens_get_tokens(tokens.get(), 0, 3, tokens_array));
+    EXPECT_FALSE(bf_tokens_get_tokens(tokens.get(), 1, 2, tokens_array));
+    EXPECT_FALSE(bf_tokens_get_tokens(tokens.get(), 2, 1, tokens_array));
+    EXPECT_FALSE(bf_tokens_get_tokens(tokens.get(), 1, -1, tokens_array));
+    EXPECT_FALSE(bf_tokens_get_tokens(tokens.get(), 1, 1, nullptr));
 }
 
 TEST(tokenizer, hello_world) {
@@ -56,16 +56,79 @@ TEST(tokenizer, hello_world) {
     std::array<bf_token, 10> token_array;
     ASSERT_TRUE(bf_tokens_get_tokens(tokens.get(), 0, token_array.size(), token_array.data()));
 
-    ASSERT_EQ(bf_token_inc_data_value, token_array[0].type);
-    ASSERT_EQ(0, token_array[0].location.line);
-    ASSERT_EQ(0, token_array[0].location.character);
+    EXPECT_EQ(bf_token_inc_data_value, token_array[0].type);
+    EXPECT_EQ(0, token_array[0].location.line);
+    EXPECT_EQ(0, token_array[0].location.character);
 
-    ASSERT_EQ(bf_token_inc_data_value, token_array[5].type);
-    ASSERT_EQ(0, token_array[5].location.line);
-    ASSERT_EQ(5, token_array[5].location.character);
+    EXPECT_EQ(bf_token_inc_data_value, token_array[5].type);
+    EXPECT_EQ(0, token_array[5].location.line);
+    EXPECT_EQ(5, token_array[5].location.character);
 
-    ASSERT_EQ(bf_token_loop_open, token_array[8].type);
-    ASSERT_EQ(0, token_array[8].location.line);
-    ASSERT_EQ(8, token_array[8].location.character);
+    EXPECT_EQ(bf_token_loop_open, token_array[8].type);
+    EXPECT_EQ(0, token_array[8].location.line);
+    EXPECT_EQ(8, token_array[8].location.character);
 }
+
+TEST(tokenizer, token_types) {
+    constexpr const char *program = "><+-.,[]";
+    unique_tokens_ptr tokens(bf_tokenize(program, nullptr, nullptr, nullptr));
+    ASSERT_NE(nullptr, tokens);
+
+    int token_count = -1;
+    ASSERT_TRUE(bf_tokens_get_token_count(tokens.get(), &token_count));
+    ASSERT_EQ(8, token_count);
+
+    std::array<bf_token, 8> token_array;
+    ASSERT_TRUE(bf_tokens_get_tokens(tokens.get(), 0, token_array.size(), token_array.data()));
+
+    EXPECT_EQ(bf_token_inc_data_ptr, token_array[0].type);
+    EXPECT_EQ(bf_token_dec_data_ptr, token_array[1].type);
+    EXPECT_EQ(bf_token_inc_data_value, token_array[2].type);
+    EXPECT_EQ(bf_token_dec_data_value, token_array[3].type);
+    EXPECT_EQ(bf_token_input, token_array[4].type);
+    EXPECT_EQ(bf_token_output, token_array[5].type);
+    EXPECT_EQ(bf_token_loop_open, token_array[6].type);
+    EXPECT_EQ(bf_token_loop_close, token_array[7].type);
+}
+
+TEST(tokenizer, other_characters_ignored) {
+    constexpr const char *program = "saa>\nsah<bwer+c-rj      \t    d.f,gjh\nhdf[e]f";
+    unique_tokens_ptr tokens(bf_tokenize(program, nullptr, nullptr, nullptr));
+    ASSERT_NE(nullptr, tokens);
+
+    int token_count = -1;
+    ASSERT_TRUE(bf_tokens_get_token_count(tokens.get(), &token_count));
+    ASSERT_EQ(8, token_count);
+
+    std::array<bf_token, 8> token_array;
+    ASSERT_TRUE(bf_tokens_get_tokens(tokens.get(), 0, token_array.size(), token_array.data()));
+
+    EXPECT_EQ(bf_token_inc_data_ptr, token_array[0].type);
+    EXPECT_EQ(bf_token_dec_data_ptr, token_array[1].type);
+    EXPECT_EQ(bf_token_inc_data_value, token_array[2].type);
+    EXPECT_EQ(bf_token_dec_data_value, token_array[3].type);
+    EXPECT_EQ(bf_token_input, token_array[4].type);
+    EXPECT_EQ(bf_token_output, token_array[5].type);
+    EXPECT_EQ(bf_token_loop_open, token_array[6].type);
+    EXPECT_EQ(bf_token_loop_close, token_array[7].type);
+}
+
+TEST(tokenizer, token_location) {
+    constexpr const char *program = ">\n<\n+\n-\n.\n,\n[\n]\n";
+    unique_tokens_ptr tokens(bf_tokenize(program, nullptr, nullptr, nullptr));
+    ASSERT_NE(nullptr, tokens);
+
+    int token_count = -1;
+    ASSERT_TRUE(bf_tokens_get_token_count(tokens.get(), &token_count));
+    ASSERT_EQ(8, token_count);
+
+    std::array<bf_token, 8> token_array;
+    ASSERT_TRUE(bf_tokens_get_tokens(tokens.get(), 0, token_array.size(), token_array.data()));
+
+    for (int i = 0; i < token_array.size(); i++) {
+        EXPECT_EQ(i, token_array[i].location.line);
+        EXPECT_EQ(0, token_array[i].location.character);
+    }
+}
+
 } // namespace
